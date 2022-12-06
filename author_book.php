1d36ea477178531f6b63d9d3dd43694f114862c9
Jessica Lejeune <ljessicalejeune@gmail.com>
2022-12-06 10:28:35 +0100

    init

---
+++ author_book.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Author {
+        public $name;
+        public $last_name;
+        public $gender;
+        public $birthdate;
+        public $books;
+    }
+
+    class Book {
+        public $title;
+        public $publication;
+        public $pages;
+        public $price;
+    }
+
+    
